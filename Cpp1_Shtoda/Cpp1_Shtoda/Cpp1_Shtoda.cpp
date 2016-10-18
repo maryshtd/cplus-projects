2cfd685d5823d0b0c1b4ae305bad559283b7c65e
Mariya Shtoda <M.Shtoda@tscrm.com>
2016-10-18 16:00:48 +0300

    first commit

--- Cpp1_Shtoda/Cpp1_Shtoda/Cpp1_Shtoda.cpp
+++ Cpp1_Shtoda/Cpp1_Shtoda/Cpp1_Shtoda.cpp
@@ -4,38 +4,11 @@
 #include "stdafx.h"
 #include "Date.h"
 #include "Menu.h"
-
+int foo(int x, int y);
 int _tmain(int argc, _TCHAR* argv[])
 {
-	setlocale(LC_ALL, "rus");
-	Date d1(1,11,2015);
-	Menu m;
-	do
-	{
-		m.Start();
-		switch (m.GetChoice())
-		{
-		case 1:
-			d1.Display();
-			break;
-		case 2:
-			d1.Display();
-			d1.TypeOfYear();
-			break;
-		case 3:
-			d1.IncreaseDate();
-			d1.Display();
-			break;
-		case 4:
-			goto end;
-			break;
-		default:
-			cout << "\n Такого варианта нет в меню программы." << endl;
-			break;
-		}
-	} while (m.Continue());
-end:
-	m.Finish();
-	return 0;
+	int h = 70000; short s = h; 
+	cout<<s;
+return 0;
 }
 
